909c8800d4dd637cad22fbf306a42eaec4403e49
true42 <bezmaternykh.alexey@gmail.com>
2022-02-23 14:03:06 +0300

    add SERVER_LOGGER in server.py

--- lesson_5/server.py
+++ lesson_5/server.py
@@ -1,13 +1,17 @@
-
+import argparse
 import json
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 import sys
-from common.variables import DEFAULT_PORT, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, \
-    RESPONDEFAULT_IP_ADDRESSSE, ERROR, MAX_CONNECTIONS
+import logging
+import log.server_log_config
+from common.variables import DEFAULT_PORT, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, MAX_CONNECTIONS
 from common.utils import send_message, get_message
+
+SERVER_LOGGER = logging.getLogger('server')
 
 
 def process_client_message(message):
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента: {message}')
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
@@ -17,29 +21,27 @@ def process_client_message(message):
     }
 
 
-def main():
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = DEFAULT_PORT
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    return parser
 
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
-    except IndexError:
-        print('После парамертра -\'a\' необходимо указать адрес, котроый будет слушать сервер.')
+
+def main():
+
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_port = namespace.p
+    listen_address = namespace.a
+
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Запуск сервера с порта {listen_port}. '
+                               f'Допустимые значения с 1024 до 65535.')
         sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
 
     transport = socket(AF_INET, SOCK_STREAM)
     transport.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -49,14 +51,17 @@ def main():
 
     while True:
         client, client_address = transport.accept()
+        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
         try:
             message_from_client = get_message(client)
-            print(message_from_client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
             response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
             send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
             client.close()
         except (ValueError, json.JSONDecodeError):
-            print('Принято некорректное сообщение.')
+            SERVER_LOGGER.error('Принято некорректное сообщение.')
             client.close()
 
 
