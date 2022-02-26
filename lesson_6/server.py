b554eb4aedab0d4857487409f66ac93c14d8f8b4
true42 <bezmaternykh.alexey@gmail.com>
2022-02-26 11:33:35 +0300

    homework_6 init

---
+++ lesson_6/server.py
@@ -0,0 +1,69 @@
+import argparse
+import json
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+import sys
+import logging
+import log.server_log_config
+from common.variables import DEFAULT_PORT, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, MAX_CONNECTIONS
+from common.utils import send_message, get_message
+
+SERVER_LOGGER = logging.getLogger('server')
+
+
+def process_client_message(message):
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента: {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    return parser
+
+
+def main():
+
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_port = namespace.p
+    listen_address = namespace.a
+
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Запуск сервера с порта {listen_port}. '
+                               f'Допустимые значения с 1024 до 65535.')
+        sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
+
+    transport = socket(AF_INET, SOCK_STREAM)
+    transport.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    transport.bind((listen_address, listen_port))
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
+        try:
+            message_from_client = get_message(client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
+            response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
+            send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            SERVER_LOGGER.error('Принято некорректное сообщение.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
