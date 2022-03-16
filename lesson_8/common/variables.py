dfe6e79ac4ce028ac45b6bc907614decb208d917
true42 <bezmaternykh.alexey@gmail.com>
2022-03-16 19:54:34 +0300

    homework_8 init

---
+++ lesson_8/common/variables.py
@@ -0,0 +1,21 @@
+import logging
+
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+LOG_LEVEL = logging.DEBUG
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+SENDER = 'sender'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONDEFAULT_IP_ADDRESSSE = 'respondefault_ip_addressse'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
